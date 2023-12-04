5cac66e66b059bcaeaca7b179bd1371fee6ee9c1
Shubham062004 <chaurasiaamrita400@gmail.com>
2023-12-04 09:44:28 +0530

    100%

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
-// Part I React 2
-    // ReactDOM.render("Hello! Welcome to React",container);
+// const container = document.getElementById('react-container');
+// ReactDOM.render("Hello! Welcome to React",container);
 
-// Part I React 3 and Part II
-    // const Container = () => {
-    //     return React.createElement(`div`,null,`Hey Kalvians! welcome learning React`,
-    //     React.createElement(`div`,null,`Let's Rock and Roll`)
-    //     )
-    // }
-
-const container = document.getElementById('react-container')
-
-// ReactDOM.render(React.createElement(Container),container)
-
-class ReactContainer extends React.Component{
-    render(){
-        return (
-            <div>Hello! Welcome to Kalvium   
-               <div>This is babel</div>
-           </div> )
-    }
+// Functional component
+const Container = () =>{
+    return React.createElement(`div`,null,`Hey Kalvians! Welcome to React Learning`,
+       React.createElement(`div`,null,`Let's rock and roll`)
+       );
 }
-ReactDOM.render(<ReactContainer/>,container);
+// Render the Container
+const container = document.getElementById('react-container');
+ReactDOM.render(React.createElement(Container),container);
