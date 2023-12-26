fd79828f4e75fb70c01cfaf5a0f8a42b9f630253
Shubham062004 <chaurasiaamrita400@gmail.com>
2023-12-26 16:41:44 +0530

    new

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-// const container = document.getElementById('react-container');
-// ReactDOM.render("Hello! Welcome to React",container);
 
-// Functional component
-const Container = () =>{
-    return React.createElement(`div`,null,`Hey Kalvians! Welcome to React Learning`,
-       React.createElement(`div`,null,`Let's rock and roll`)
-       );
+// let container= document.getElementById("react-container");
+// ReactDOM.render("Hello! Welcome to React",container)
+// container.innerHTML="Hello! Welcome to JS"
+
+// function kalviclass (){
+//         return (
+//             React.createElement(
+//                 'div',
+//                 {id:"h1-tag"},
+//                 'Hey Kalvians! Welcome to React Learning',
+//                 React.createElement('div',null,`Let's rock and roll`))  //(tag_name,props,children(message))
+//          )
+//         }
+
+// ReactDOM.render(kalviclass() ,container)
+
+class klaviclass extends React.Component{
+    render(){
+        return (
+                    React.createElement(
+                    'div',
+                    {id:"h1-tag"},
+                    'Hey Kalvians',
+                    React.createElement('div',null,`Let's rock and roll with classes`))  //(tag_name,props,children(message))
+                )
+
+
+
+            }
 }
-// Render the Container
-const container = document.getElementById('react-container');
-ReactDOM.render(React.createElement(Container),container);
+
+let container= document.getElementById("react-container");
+ReactDOM.render(React.createElement(klaviclass),container)
